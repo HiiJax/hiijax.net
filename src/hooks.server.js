44fe7a0bbc2bcd2cf12e1014037e0fba1a0d146e
Jax <git@hiijax.net>
2026-03-06 14:05:21 -0500

    too aggressive lol

--- src/hooks.server.js
+++
@@ -1,7 +0,0 @@
-import { readFile, writeFile } from "fs/promises";
-
-/** @type {import('@sveltejs/kit').HandleServerError} */
-export async function handleError() {
-    const hits = Number(await readFile('data/hits', 'utf8')) - 1;
-    writeFile('data/hits', String(hits))
-}
